73ac7f76cf70082e8b5291165c85d5ed8fce3387
Kareeeels <zs24021118@estudiantes.uv.mx>
2026-07-02 10:11:03 -0600

    Cambiar backend a Node.js

--- backend/src/main/java/com/computadoras/repository/ComputadoraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.computadoras.repository;
-
-import com.computadoras.model.Computadora;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComputadoraRepository extends JpaRepository<Computadora, Long> {
-}
